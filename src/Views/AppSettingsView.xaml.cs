89073ee93d2eb3988406b36ef0dc448ee44e3aef
Balkisteir <shadow.gaming.vid@gmail.com>
2026-03-14 21:01:21 -0400

    Handle Hyperlink navigation in AppSettingsView
    Add a RequestNavigate event handler to AppSettingsView that opens hyperlink URIs using Process.Start with UseShellExecute=true and marks the event handled. Also add necessary using directives (System.Diagnostics and System.Windows.Navigation). This enables in-app hyperlinks to open in the user's default browser.

--- src/Views/AppSettingsView.xaml.cs
+++ src/Views/AppSettingsView.xaml.cs
@@ -1,4 +1,6 @@
+using System.Diagnostics;
 using System.Windows.Controls;
+using System.Windows.Navigation;
 
 namespace VRCGroupTools.Views;
 
@@ -7,5 +9,16 @@ public partial class AppSettingsView : UserControl
     public AppSettingsView()
     {
         InitializeComponent();
+    }
+
+    private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+    {
+        Process.Start(new ProcessStartInfo
+        {
+            FileName = e.Uri.AbsoluteUri,
+            UseShellExecute = true
+        });
+
+        e.Handled = true;
     }
 }
